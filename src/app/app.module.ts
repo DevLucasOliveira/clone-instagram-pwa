fd458584c7e7b7bc9362c45217f603be4419b00d
DevLucasOliveira <lucasoliveira.si@outlook.com>
2021-01-04 06:09:49 -0300

    Estruturando páginas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,27 @@
+import { environment } from './../environments/environment';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
-
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { AngularFireModule } from '@angular/fire';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent
+  ],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule, 
+    IonicModule.forRoot(), 
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase)  
+  ],
   providers: [
-    StatusBar,
-    SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    { 
+      provide: RouteReuseStrategy, 
+      useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
 })
---
+++ src/app/models/post.model.ts
@@ -0,0 +1,11 @@
+export class Post {
+
+    constructor(
+        public image: string = '',
+        public description: string = '',
+        public location: string = ''
+    ) {
+
+    }
+
+}
